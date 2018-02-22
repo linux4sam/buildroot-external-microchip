a3166770fcd17f86a861d1ee5f955ba26f69371b
Joshua Henderson <joshua.henderson@microchip.com>
2018-02-22 13:59:03 -0700

    gst1-at91: new package
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ package/gst1-at91/gst1-at91.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# gst1-at91
+#
+################################################################################
+
+GST1_AT91_VERSION = 50941639a3b1c287ffc28baa5b786c2c00a9494b
+GST1_AT91_SITE = https://github.com/linux4sam/gst1-hantro-g1
+GST1_AT91_SITE_METHOD = git
+GST1_AT91_LICENSE = LGPLv2+
+GST1_AT91_LICENSE_FILES = COPYING.LESSER
+GST1_AT91_INSTALL_STAGING = YES
+GST1_AT91_GIT_SUBMODULES = YES
+
+GST1_AT91_DEPENDENCIES += \
+	host-pkgconf \
+	gstreamer1 \
+	gst1-plugins-base \
+	g1-binaries \
+	libdrm
+
+define GST1_AT91_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh --noconfigure
+endef
+
+GST1_AT91_POST_PATCH_HOOKS += GST1_AT91_RUN_AUTOGEN
+GST1_AT91_DEPENDENCIES += host-automake host-autoconf host-libtool
+
+GST1_AT91_CONF_ENV = \
+		G1_CFLAGS="-I${STAGING_DIR}/usr/include" \
+		G1_LIBS="-L${STAGING_DIR}/usr/lib" \
+		GST_LIBDRM="${STAGING_DIR}/"
+
+define GST1_AT91_BUILD_CMDS
+	cd $(@D); $(TARGET_MAKE_ENV) make GST_LIBDRM="$(STAGING_DIR)"
+endef
+
+$(eval $(autotools-package))
