4bbdbe1e88787ea3a187eb45c719912de7481357
Joshua Henderson <joshua.henderson@microchip.com>
2018-02-28 08:56:32 -0700

    python-mpio: bump version and remove iocontrol
    This involves a version bump that no longer includes the iocontrol application.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/python-mpio/python-mpio.mk
+++ package/python-mpio/python-mpio.mk
@@ -4,23 +4,19 @@
 #
 ################################################################################
 
-PYTHON_MPIO_VERSION = 1ae75341c8e81394ecc67edae596bba2b06e4bcd
+PYTHON_MPIO_VERSION = d329ac453cc75d1483e516f7bfab6e8727675dc4
 PYTHON_MPIO_SITE = https://github.com/linux4sam/mpio
 PYTHON_MPIO_SITE_METHOD = git
-PYTHON_MPIO_LICENSE = Apache-2.0 (libraries), GPL-3.0-only (programs)
-PYTHON_MPIO_LICENSE_FILES = LICENSE LICENSE.MIT iocontrol/LICENSE iocontrol/LICENSE.MIT
+PYTHON_MPIO_LICENSE = Apache-2.0, MIT
+PYTHON_MPIO_LICENSE_FILES = LICENSE LICENSE.MIT
 PYTHON_MPIO_SETUP_TYPE = setuptools
-PYTHON_MPIO_DEPENDENCIES = python-pyqt5
 
-define PYTHON_MPIO_INSTALL_MENU
-        $(INSTALL) -m 0644 -D $(@D)/iocontrol/res/mpio.png \
-                $(TARGET_DIR)/opt/ApplicationLauncher/applications/resources/mpio.png
-        $(INSTALL) -m 0644 -D $(@D)/iocontrol/res/08-iocontrol.xml \
-                $(TARGET_DIR)/opt/ApplicationLauncher/applications/xml/08-iocontrol.xml
-        $(INSTALL) -m 0755 -D $(@D)/iocontrol/res/iocontrol.sh \
-                $(TARGET_DIR)/opt/iocontrol/iocontrol.sh
-endef
+ifeq ($(BR2_PACKAGE_PYTHON),y)
+PYTHON_MPIO_DEPENDENCIES += python
+endif
 
-PYTHON_MPIO_POST_INSTALL_TARGET_HOOKS += PYTHON_MPIO_INSTALL_MENU
+ifeq ($(BR2_PACKAGE_PYTHON3),y)
+PYTHON_MPIO_DEPENDENCIES += python3
+endif
 
 $(eval $(python-package))
