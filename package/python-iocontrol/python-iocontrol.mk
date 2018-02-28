ca2e331d0650c4d27d8ac1b50c0d50da9661de6a
Joshua Henderson <joshua.henderson@microchip.com>
2018-02-28 08:58:58 -0700

    iocontrol: new package
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ package/python-iocontrol/python-iocontrol.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# python-iocontrol
+#
+################################################################################
+
+PYTHON_IOCONTROL_VERSION = bb6d6a3f3340b3b43b41bf2232e52cc3d2c01ac4
+PYTHON_IOCONTROL_SITE = https://github.com/linux4sam/iocontrol
+PYTHON_IOCONTROL_SITE_METHOD = git
+PYTHON_IOCONTROL_LICENSE = GPL-3.0-only (programs)
+PYTHON_IOCONTROL_LICENSE_FILES = LICENSE LICENSE.MIT
+PYTHON_IOCONTROL_SETUP_TYPE = setuptools
+PYTHON_IOCONTROL_DEPENDENCIES = python-pyqt5 python-mpio
+
+define PYTHON_IOCONTROL_INSTALL_MENU
+        $(INSTALL) -m 0644 -D $(@D)/res/iocontrol.png \
+                $(TARGET_DIR)/opt/ApplicationLauncher/applications/resources/iocontrol.png
+        $(INSTALL) -m 0644 -D $(@D)/res/08-iocontrol.xml \
+                $(TARGET_DIR)/opt/ApplicationLauncher/applications/xml/08-iocontrol.xml
+        $(INSTALL) -m 0755 -D $(@D)/res/iocontrol.sh \
+                $(TARGET_DIR)/opt/iocontrol/iocontrol.sh
+endef
+
+PYTHON_IOCONTROL_POST_INSTALL_TARGET_HOOKS += PYTHON_IOCONTROL_INSTALL_MENU
+
+$(eval $(python-package))
