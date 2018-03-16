6e849c2491a30133e5d24bb379269ff1625349fe
Joshua Henderson <joshua.henderson@microchip.com>
2018-03-16 15:49:30 -0700

    python-mpio: use the github function for download
    Prefer this over using the static URL.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/python-mpio/python-mpio.mk
+++ package/python-mpio/python-mpio.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 PYTHON_MPIO_VERSION = d329ac453cc75d1483e516f7bfab6e8727675dc4
-PYTHON_MPIO_SITE = https://github.com/linux4sam/mpio
-PYTHON_MPIO_SITE_METHOD = git
+PYTHON_MPIO_SITE = $(call github,linux4sam,mpio,$(PYTHON_MPIO_VERSION))
 PYTHON_MPIO_LICENSE = Apache-2.0, MIT
 PYTHON_MPIO_LICENSE_FILES = LICENSE LICENSE.MIT
 PYTHON_MPIO_SETUP_TYPE = setuptools
