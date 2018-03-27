e4f7abd2083db84446fa22c256583a160c3361cf
Joshua Henderson <joshua.henderson@microchip.com>
2018-03-27 10:28:47 -0700

    minehunt: change version to release tag
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/minehunt/minehunt.mk
+++ package/minehunt/minehunt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MINEHUNT_VERSION = 0bc4b8273dc828827d0e591053a84213c999434c
+MINEHUNT_VERSION = v1.7
 MINEHUNT_SITE = $(call github,linux4sam,minehunt,$(MINEHUNT_VERSION))
 MINEHUNT_LICENSE = LGPL-2.1-only, GPL-3.0-only
 MINEHUNT_DEPENDENCIES = qt5base qt5quick1d
