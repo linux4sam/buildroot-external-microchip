a972c4c464eb16c6acbbbc936fe22abeb5612dfb
Joshua Henderson <joshua.henderson@microchip.com>
2018-03-27 14:20:48 -0700

    libplanes: bump version to v0.0.3
    Minor update that includes a demo script fix.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/libplanes/libplanes.mk
+++ package/libplanes/libplanes.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPLANES_VERSION = v0.0.2
+LIBPLANES_VERSION = v0.0.3
 LIBPLANES_SITE = $(call github,linux4sam,libplanes,$(LIBPLANES_VERSION))
 LIBPLANES_LICENSE = MIT
 LIBPLANES_LICENSE_FILES = COPYING
