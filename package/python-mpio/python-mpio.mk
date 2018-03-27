61e4241599a268b89a715c0defb28ae80eb4de91
Joshua Henderson <joshua.henderson@microchip.com>
2018-03-27 10:29:21 -0700

    python-mpio: change version to release tag
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/python-mpio/python-mpio.mk
+++ package/python-mpio/python-mpio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MPIO_VERSION = d329ac453cc75d1483e516f7bfab6e8727675dc4
+PYTHON_MPIO_VERSION = v1.0
 PYTHON_MPIO_SITE = $(call github,linux4sam,mpio,$(PYTHON_MPIO_VERSION))
 PYTHON_MPIO_LICENSE = Apache-2.0, MIT
 PYTHON_MPIO_LICENSE_FILES = LICENSE LICENSE.MIT
