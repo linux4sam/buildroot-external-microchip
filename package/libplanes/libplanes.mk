15d8f49605bf66eac3ecf31f4935c86006c55463
Joshua Henderson <joshua.henderson@microchip.com>
2018-09-04 09:43:18 -0700

    libplanes: bump version
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/libplanes/libplanes.mk
+++ package/libplanes/libplanes.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPLANES_VERSION = v0.0.3
+LIBPLANES_VERSION = 6d65d1df800188f2f1e31518a6b35629a792e35d
 LIBPLANES_SITE = $(call github,linux4sam,libplanes,$(LIBPLANES_VERSION))
 LIBPLANES_LICENSE = MIT
 LIBPLANES_LICENSE_FILES = COPYING
