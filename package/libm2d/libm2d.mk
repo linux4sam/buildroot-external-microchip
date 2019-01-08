639f70e241ed40a54a90840de5bf86b0bd1243aa
Joshua Henderson <joshua.henderson@microchip.com>
2019-01-08 08:49:09 -0700

    package: libm2d: new package
    libm2d is a library that provides the Microchip 2D Graphics API.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ package/libm2d/libm2d.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# libm2d
+#
+################################################################################
+
+LIBM2D_VERSION = sam9x60_2.0
+LIBM2D_SOURCE = libm2d-$(LIBM2D_VERSION).tar.gz
+LIBM2D_SITE = ftp://at91.com/pub/src
+LIBM2D_STRIP_COMPONENTS = 0
+LIBM2D_LICENSE = MIT
+LIBM2D_LICENSE_FILES = COPYING
+LIBM2D_AUTORECONF = YES
+LIBM2D_AUTORECONF_OPTS += -I $(HOST_DIR)/usr/share/autoconf-archive
+LIBM2D_DEPENDENCIES = libdrm \
+	host-pkgconf host-autoconf-archive
+LIBM2D_INSTALL_STAGING = YES
+
+LIBM2D_CONF_OPTS = \
+	--disable-cairo
+
+define LIBM2D_RUN_AUTOGEN
+        cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+LIBM2D_POST_PATCH_HOOKS += LIBM2D_RUN_AUTOGEN
+
+$(eval $(autotools-package))
