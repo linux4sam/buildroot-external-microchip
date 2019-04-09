2478194d7fc1140be1a89737c50ac59a7af33fe4
Claudiu Beznea <claudiu.beznea@microchip.com>
2019-04-09 12:56:49 +0300

    wilc_firmware: add package
    Add package for WILC firmwares.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

---
+++ package/wilc_firmware/wilc_firmware.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# wireless_firmware
+#
+################################################################################
+
+WILC_FIRMWARE_VERSION = wilc_linux_15_2
+WILC_FIRMWARE_SITE = https://github.com/linux4wilc/firmware.git
+WILC_FIRMWARE_SITE_METHOD = git
+WILC_FIRMWARE_LICENSE = Microchip firmware
+WILC_FIRMWARE_LICENSE_FILES = LICENCE.wilc_fw
+
+define WILC_FIRMWARE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/lib/firmware/mchp
+	$(INSTALL) -D -m 0644 $(@D)/* $(TARGET_DIR)/lib/firmware/mchp
+
+	rm -f ${TARGET_DIR}/lib/firmware/mchp/README.md
+	rm -rf ${TARGET_DIR}/lib/firmware/mchp/LICENCE.wilc_fw
+	chmod -x ${TARGET_DIR}/lib/firmware/mchp/*
+endef
+
+$(eval $(generic-package))
