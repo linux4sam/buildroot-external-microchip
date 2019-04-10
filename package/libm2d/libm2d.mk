dc9ca31fc305f85d8a5721ff3655180e0cbd4110
Tudor Ambarus <tudor.ambarus@microchip.com>
2019-04-10 15:29:04 +0300

    package: libm2d: use public version of libm2d
    use ftp to get it from:
    ftp://at91.com/pub/src/libm2d-sam9x60_2.0.tar.gz
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- package/libm2d/libm2d.mk
+++ package/libm2d/libm2d.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-LIBM2D_VERSION = 90b2afe9d69e74c211eecf90e742b113935f7441
-LIBM2D_SITE = https://bitbucket.microchip.com/scm/~c16205/libm2d.git
-LIBM2D_SITE_METHOD = git
+LIBM2D_VERSION = sam9x60_2.0
+LIBM2D_SOURCE = libm2d-$(LIBM2D_VERSION).tar.gz
+LIBM2D_SITE = ftp://at91.com/pub/src
+LIBM2D_STRIP_COMPONENTS = 0
 LIBM2D_LICENSE = MIT
 LIBM2D_LICENSE_FILES = COPYING
 LIBM2D_AUTORECONF = YES
