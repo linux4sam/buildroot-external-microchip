eb2b81638990a0fa3bc5fac9ac0d8ae3a347729f
Prasanthi Chellakumar <Prasanthi.Chellakumar@microchip.com>
2019-05-22 11:50:51 -0700

    egt-media: new package for media files required by egt examples
    Signed-off-by: Prasanthi Chellakumar <Prasanthi.Chellakumar@microchip.com>

---
+++ package/egt-media/egt-media.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# Media files for EGT example applications
+#
+################################################################################
+
+EGT_MEDIA_VERSION = 1.0
+EGT_MEDIA_SITE = https://microchiptechnology-my.sharepoint.com/:u:/g/personal/prasanthi_chellakumar_microchip_com
+EGT_MEDIA_SOURCE = EZ9qNp_YhtZLocY-AzDLk2wBZ4fcT8FqY4zEpk6FjFSvRA?download=1
+EGT_MEDIA_SITE_METHOD = wget
+EGT_MEDIA_FILENAME = $(PKG)-$(EGT_MEDIA_VERSION).tar.gz
+
+define EGT_MEDIA_RENAME_DOWNLOADS
+	mv $(DL_DIR)/$(EGT_MEDIA_SOURCE) $(DL_DIR)/$(EGT_MEDIA_FILENAME)
+endef
+
+EGT_MEDIA_POST_DOWNLOAD_HOOKS = EGT_MEDIA_RENAME_DOWNLOADS
+
+define EGT_MEDIA_EXTRACT_CMDS
+endef
+
+define EGT_MEDIA_INSTALL_TARGET_CMDS
+        $(call suitable-extractor, $(EGT_MEDIA_FILENAME)) \
+		$(DL_DIR)/$(EGT_MEDIA_FILENAME) | \
+                $(TAR) --strip-components=1 -C  $(TARGET_DIR)/usr/share/egt/examples $(TAR_OPTIONS) -
+endef
+
+$(eval $(generic-package))
