edf470ea3cad97a918fd57f037aa875c1c9f5b17
Prasanthi Chellakumar <Prasanthi.Chellakumar@microchip.com>
2019-05-22 11:50:51 -0700

    egt-media: new package for media files required by egt examples
    Signed-off-by: Prasanthi Chellakumar <Prasanthi.Chellakumar@microchip.com>

---
+++ package/egt-media/egt-media.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# Media files for EGT example applications
+#
+################################################################################
+
+
+EGT_MEDIA_VERSION = 1.1
+EGT_MEDIA_SITE = ftp://www.at91.com/pub/demo/media
+EGT_MEDIA_SOURCE = egt-media-${EGT_MEDIA_VERSION}.tar.gz
+EGT_MEDIA_LICENSE = Apache-2.0
+EGT_MEDIA_INSTALL_TARGET = YES
+EGT_MEDIA_INSTALL_STAGING = YES
+
+define EGT_MEDIA_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/egt/examples
+        $(call suitable-extractor, $(EGT_MEDIA_SOURCE)) \
+		$(EGT_MEDIA_DL_DIR)/$(EGT_MEDIA_SOURCE) | \
+                $(TAR) --strip-components=1 -C  $(TARGET_DIR)/usr/share/egt/examples $(TAR_OPTIONS) -
+endef
+
+$(eval $(generic-package))
