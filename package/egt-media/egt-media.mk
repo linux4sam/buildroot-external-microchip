64026b1a027808a04813bae7505eb4d2499bc75e
Sandeep Sheriker M <sandeep.sheriker@microchip.com>
2019-07-11 14:41:19 -0700

    package/egt-media: fetch egt-media files from ftp server
    fetching egt-media files from ftp server and update md5 hash.
    
    Signed-off-by: Sandeep Sheriker M <sandeep.sheriker@microchip.com>

--- package/egt-media/egt-media.mk
+++ package/egt-media/egt-media.mk
@@ -4,25 +4,18 @@
 #
 ################################################################################
 
+
 EGT_MEDIA_VERSION = 1.0
-EGT_MEDIA_SITE = https://microchiptechnology-my.sharepoint.com/:u:/g/personal/sandeepsheriker_mallikarjun_microchip_com
-EGT_MEDIA_SOURCE = EYh2YH8-dNpDgZhXjmkSvHYBM4KXhHSTzHzeYqr77MYzBw?download=1
-EGT_MEDIA_SITE_METHOD = wget
-EGT_MEDIA_FILENAME = $(PKG)-$(EGT_MEDIA_VERSION).tar.gz
-
-define EGT_MEDIA_RENAME_DOWNLOADS
-	mv $(DL_DIR)/egt-media/$(EGT_MEDIA_SOURCE) $(DL_DIR)/$(EGT_MEDIA_FILENAME)
-endef
-
-EGT_MEDIA_POST_DOWNLOAD_HOOKS = EGT_MEDIA_RENAME_DOWNLOADS
-
-define EGT_MEDIA_EXTRACT_CMDS
-endef
+EGT_MEDIA_SITE = ftp://www.at91.com/pub/demo/media
+EGT_MEDIA_SOURCE = egt-media-${EGT_MEDIA_VERSION}.tar.gz
+EGT_MEDIA_LICENSE = Apache-2.0
+EGT_MEDIA_INSTALL_TARGET = YES
+EGT_MEDIA_INSTALL_STAGING = YES
 
 define EGT_MEDIA_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/egt/examples
-        $(call suitable-extractor, $(EGT_MEDIA_FILENAME)) \
-		$(DL_DIR)/$(EGT_MEDIA_FILENAME) | \
+        $(call suitable-extractor, $(EGT_MEDIA_SOURCE)) \
+		$(EGT_MEDIA_DL_DIR)/$(EGT_MEDIA_SOURCE) | \
                 $(TAR) --strip-components=1 -C  $(TARGET_DIR)/usr/share/egt/examples $(TAR_OPTIONS) -
 endef
 
