9d1d1520e0880480cec283f23fe568faa52a5336
Joshua Henderson <joshua.henderson@microchip.com>
2019-07-12 16:47:19 -0700

    egt-launcher: support init script and bump version
    Adds support for choosing the EGT launcher as a demo init launcher.
    
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/egt-launcher/egt-launcher.mk
+++ package/egt-launcher/egt-launcher.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EGT_LAUNCHER_VERSION = 92b47c1cd8f0447ff8f6ecea720c93dbaba4661c
+EGT_LAUNCHER_VERSION = bef23e3d398df815c20116c1636991f4915c688d
 EGT_LAUNCHER_SITE = https://bitbucket.microchip.com/scm/linux4sam/egt-launcher.git
 EGT_LAUNCHER_SITE_METHOD = git
 EGT_LAUNCHER_GIT_SUBMODULES = YES
@@ -16,5 +16,14 @@ define EGT_LAUNCHER_RUN_AUTOGEN
         cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
 endef
 EGT_LAUNCHER_POST_PATCH_HOOKS += EGT_LAUNCHER_RUN_AUTOGEN
+
+define EGT_LAUNCHER_INSTALL_INIT
+        $(INSTALL) -m 0755 -D $(EGT_LAUNCHER_PKGDIR)/S99demo \
+                $(TARGET_DIR)/etc/init.d/S99demo
+endef
+
+ifeq ($(BR2_PACKAGE_EGT_LAUNCHER_INIT),y)
+EGT_LAUNCHER_POST_INSTALL_TARGET_HOOKS += EGT_LAUNCHER_INSTALL_INIT
+endif
 
 $(eval $(autotools-package))
