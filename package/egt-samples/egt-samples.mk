9727a4c7fdd0d64d2fcf006cc6bae9ac1ad5d15a
Joshua Henderson <joshua.henderson@microchip.com>
2019-07-12 17:50:28 -0700

    egt-samples: bump version and install with egt_ prefix
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/egt-samples/egt-samples.mk
+++ package/egt-samples/egt-samples.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-EGT_SAMPLES_VERSION = 22d9a88a4ebc549448aed040fd2e00d5a0ccb4b8
+EGT_SAMPLES_VERSION = beda0b38d31d125ac9999a3c8d6c363d147123b8
 EGT_SAMPLES_SITE = https://bitbucket.microchip.com/scm/linux4sam/egt-samples.git
 EGT_SAMPLES_SITE_METHOD = git
 EGT_SAMPLES_GIT_SUBMODULES = YES
 EGT_SAMPLES_LICENSE = Apache-2.0
 EGT_SAMPLES_INSTALL_TARGET = YES
 EGT_SAMPLES_DEPENDENCIES = egt
+
+EGT_SAMPLES_CONF_OPTS = --program-prefix='egt_'
 
 define EGT_SAMPLES_RUN_AUTOGEN
         cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
