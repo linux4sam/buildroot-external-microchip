b0197eb6219d518909b0153af7332dcdd145fd27
Eugen Hristev <eugen.hristev@microchip.com>
2019-09-24 15:44:21 +0300

    package: wildwest: bump version
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- package/wildwest/wildwest.mk
+++ package/wildwest/wildwest.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WILDWEST_VERSION = 69114abeb72380cdec8b7f7e80b94ac0dd77036e
+WILDWEST_VERSION = 0b1b22e3eb5c819983f3d14696214ebaabf28223
 WILDWEST_SITE = $(call github,linux4sam,wildwest,$(WILDWEST_VERSION))
 WILDWEST_LICENSE = Apache-2.0
 WILDWEST_LICENSE_FILES = COPYING
