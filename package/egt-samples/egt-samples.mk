b26659c3f3f5b2b25b4b8ac5cd8af02e716068f4
Joshua Henderson <joshua.henderson@microchip.com>
2019-09-25 11:08:34 +0300

    egt-samples: add new package
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ package/egt-samples/egt-samples.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# egt sample applications
+#
+################################################################################
+
+EGT_SAMPLES_VERSION = egt_0.8
+EGT_SAMPLES_SITE = https://github.com/linux4sam/egt-samples.git
+EGT_SAMPLES_SITE_METHOD = git
+EGT_SAMPLES_GIT_SUBMODULES = YES
+EGT_SAMPLES_LICENSE = Apache-2.0
+EGT_SAMPLES_INSTALL_TARGET = YES
+EGT_SAMPLES_DEPENDENCIES = egt
+
+EGT_SAMPLES_CONF_OPTS = --program-prefix='egt_'
+
+define EGT_SAMPLES_RUN_AUTOGEN
+        cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+EGT_SAMPLES_POST_PATCH_HOOKS += EGT_SAMPLES_RUN_AUTOGEN
+
+$(eval $(autotools-package))
