5c79900e4ad493d0350996aab6aaa4ab2b9afd14
Eugen Hristev <eugen.hristev@microchip.com>
2019-10-01 16:45:12 +0300

    package: python-iocontrol: integrate with egt-launcher
    Prepare integration with egt-launcher
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- package/python-iocontrol/python-iocontrol.mk
+++ package/python-iocontrol/python-iocontrol.mk
@@ -13,9 +13,9 @@ PYTHON_IOCONTROL_DEPENDENCIES = python-pyqt5 python-mpio
 
 define PYTHON_IOCONTROL_INSTALL_MENU
         $(INSTALL) -m 0644 -D $(@D)/res/iocontrol.png \
-                $(TARGET_DIR)/opt/ApplicationLauncher/applications/resources/iocontrol.png
-        $(INSTALL) -m 0644 -D $(@D)/res/08-iocontrol.xml \
-                $(TARGET_DIR)/opt/ApplicationLauncher/applications/xml/08-iocontrol.xml
+                $(TARGET_DIR)/opt/applications/resources/iocontrol.png
+	 $(INSTALL) -m 0755 -D $(@D)/res/08-iocontrol.xml \
+		$(TARGET_DIR)/opt/applications/resources/08-iocontrol.xml
         $(INSTALL) -m 0755 -D $(@D)/res/iocontrol.sh \
                 $(TARGET_DIR)/opt/iocontrol/iocontrol.sh
 endef
