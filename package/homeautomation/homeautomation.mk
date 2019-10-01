e17a6a39fe80c10a3aed84e6e4f43213ad3c9376
Eugen Hristev <eugen.hristev@microchip.com>
2019-10-01 16:44:07 +0300

    package: homeautomation: integrated with egt-launcher
    Prepare integration with egt-launcher
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- package/homeautomation/homeautomation.mk
+++ package/homeautomation/homeautomation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOMEAUTOMATION_VERSION = ebb179cd3787a572c3423880768ed6d056f61b2a
+HOMEAUTOMATION_VERSION = 7e3f384de01fb2ad3a7b8b6a05acb0d8d68e9508
 HOMEAUTOMATION_SITE = $(call github,linux4sam,home-automation,$(HOMEAUTOMATION_VERSION))
 HOMEAUTOMATION_LICENSE = Atmel LIMITED License Agreement
 HOMEAUTOMATION_DEPENDENCIES = qt5base libv4l
@@ -20,6 +20,11 @@ endef
 define HOMEAUTOMATION_INSTALL_TARGET_CMDS
 	$(MAKE) INSTALL_ROOT=$(TARGET_DIR) -C $(@D) install
 	$(INSTALL) -m 0755 $(@D)/HomeAutomation $(TARGET_DIR)/opt/HomeAutomation/
+	$(INSTALL) -D -m 0664 $(@D)/configs/home-automation.png \
+		$(TARGET_DIR)/opt/applications/resources/home-automation.png
+	 $(INSTALL) -m 0755 -D $(@D)/configs/01-home-automation.xml \
+		$(TARGET_DIR)/opt/applications/resources/01-home-automation.xml
+
 endef
 
 $(eval $(generic-package))
