a3856f828fbc2b9632e48ab9b85549fad2b8f1c1
Sandeep Sheriker M <sandeep.sheriker@microchip.com>
2019-10-04 17:20:54 -0700

    plplot: add new package
    Signed-off-by: Sandeep Sheriker M <sandeep.sheriker@microchip.com>
    [reword commit message]
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

---
+++ package/plplot/plplot.mk
@@ -0,0 +1,41 @@
+################################################################################
+#
+# gnuplot
+#
+################################################################################
+
+PLPLOT_VERSION = plplot-5.13.0
+PLPLOT_SITE = git://git.code.sf.net/p/plplot/plplot
+PLPLOT_LICENSE = GPL
+PLPLOT_LICENSE_FILES = Copyright
+PLPLOT_INSTALL_STAGING = YES
+PLPLOT_SUPPORTS_IN_SOURCE_BUILD = NO
+
+PLPLOT_DEPENDENCIES += \
+        host-pkgconf \
+	freetype \
+	fontconfig \
+	cairo \
+	pango \
+	qhull \
+	shapelib 
+
+PLPLOT_CONF_OPTS += -DDEFAULT_NO_BINDINGS=ON -DDEFAULT_NO_CAIRO_DEVICES=OFF -DENABLE_cxx=ON -DNaNAwareCCompiler=ON -DPL_DOUBLE=OFF -DCMAKE_NATIVE_BINARY_DIR=${@D}/buildroot-build -DCMAKE_VERBOSE_MAKEFILE=ON
+
+#        libglib2 \
+#-DCMAKE_VERBOSE_MAKEFILE=ON -DBUILD_EXAMPLE=ON -DBUILD_EXAMPLES=ON -DCMAKE_INSTALL_RPATH="${@D}/buildroot-build/lib"
+
+# This fix is avoid configure error of missing genereated files hence manually copying
+# these generated files from host
+define PLPLOT_COPY_GEN_FILES
+	mkdir -p ${@D}/buildroot-build
+	mkdir -p ${@D}/buildroot-build/lib/qsastime
+	mkdir -p ${@D}/buildroot-build/include
+	cp -avf $(PLPLOT_PKGDIR)/deltaT.h  ${@D}/buildroot-build/lib/qsastime/
+	cp -avf $(PLPLOT_PKGDIR)/tai-utc.h ${@D}/buildroot-build/lib/qsastime/
+	cp -avf $(PLPLOT_PKGDIR)/plhershey-unicode.h ${@D}/buildroot-build/include/
+endef
+PLPLOT_PRE_CONFIGURE_HOOKS += PLPLOT_COPY_GEN_FILES
+
+$(eval $(cmake-package))
+#$(eval $(host-cmake-package))
