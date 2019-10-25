47825a28f0999ebaaa35ff7b4af597a854a7753e
Ludovic Desroches <ludovic.desroches@microchip.com>
2019-10-25 13:23:52 +0200

    package: add egt-benchmark
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>

---
+++ package/egt-benchmark/egt-benchmark.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# egt benchmark
+#
+################################################################################
+
+EGT_BENCHMARK_VERSION = d8abe845c49597a652f250ec9564538092cfcda1
+EGT_BENCHMARK_SITE = https://github.com/linux4sam/egt-benchmark.git
+EGT_BENCHMARK_SITE_METHOD = git
+EGT_BENCHMARK_GIT_SUBMODULES = YES
+EGT_BENCHMARK_LICENSE = Apache-2.0
+EGT_BENCHMARK_INSTALL_TARGET = YES
+EGT_BENCHMARK_DEPENDENCIES = egt
+
+EGT_BENCHMARK_CONF_OPTS = --program-prefix='egt_'
+
+define EGT_BENCHMARK_RUN_AUTOGEN
+        cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+EGT_BENCHMARK_POST_PATCH_HOOKS += EGT_BENCHMARK_RUN_AUTOGEN
+
+$(eval $(autotools-package))
