f871e9abbd986444fd564eb5d8668e11d8a14585
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-02-14 16:56:59 +0200

    board: sama5d2_xult_greenkey: Update greengrass start script
    If NOT using systemd, we have to setup Cgroups and to enable
    link protection.
    
    Suggested-by: Larry Gass <larry.gass@microchip.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- board/sama5d2_xult_greenkey/rootfs-overlay/root/start.sh
+++ board/sama5d2_xult_greenkey/rootfs-overlay/root/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0
 
-/greengrass/ggc/core/greengrassd start
+# Setup Cgroups
+mount -t cgroup -o all cgroup /sys/fs/cgroup
+echo 1 > /sys/fs/cgroup/memory.use_hierarchy
 
+# Enable link protection
+/etc/init.d/S02sysctl start
+sysctl -w fs.protected_hardlinks=1
+sysctl -w fs.protected_symlinks=1
+
+/greengrass/ggc/core/greengrassd start
