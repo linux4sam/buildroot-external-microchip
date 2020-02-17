1d4f553b6d571b015c1a1112e995fc4eea505bdc
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-02-17 19:14:10 +0200

    package: python-cryptoauthlib: Bump to 20200208 version
    Update to latest version of https://pypi.org/project/cryptoauthlib.
    
    20200208 fixes the wheel dependency, remove the python-cryptauthlib
    patch.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- package/python-cryptoauthlib/python-cryptoauthlib.mk
+++ package/python-cryptoauthlib/python-cryptoauthlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CRYPTOAUTHLIB_VERSION = 20190304
+PYTHON_CRYPTOAUTHLIB_VERSION = 20200208
 PYTHON_CRYPTOAUTHLIB_SOURCE = cryptoauthlib-$(PYTHON_CRYPTOAUTHLIB_VERSION).tar.gz
-PYTHON_CRYPTOAUTHLIB_SITE = https://pypi.python.org/packages/a6/b9/698260893ad00c0a730012c1ef7e22c7dc214d219af790069a57a48fbf0f
+PYTHON_CRYPTOAUTHLIB_SITE = https://files.pythonhosted.org/packages/1b/ed/da1709095abd203e37892f7183b68433382ce6a0f6129dfa0ac47e2ba85c
 PYTHON_CRYPTOAUTHLIB_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
