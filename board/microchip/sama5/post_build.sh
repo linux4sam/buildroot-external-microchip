dee28fb3e870add9b0c49ef0329ee919f0e4c366
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-02-25 13:44:23 +0200

    configs: sama5d2_xplained_graphics: Add python3.8
    Needed by the AWS IoT Greengrass Lambda functions, they are authored
    in the following languages:
        Python 2.7 and 3.7
    
    Include Python2.7 and Python3.8 in the rootfs.
    
    Update post_build.sh script to use python3.8.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- board/microchip/sama5/post_build.sh
+++ board/microchip/sama5/post_build.sh
@@ -4,4 +4,4 @@
 echo "conf=${BR2_CONFIG}, hd=${HOST_DIR}, sd=${STAGING_DIR}, td=${TARGET_DIR}, bd=${BUILD_DIR}, bindir=${BINARIES_DIR}, base=${BASE_DIR}"
 echo "Copying libcryptoauth.so to python2.7/site-packages"
 
-ln -sf /usr/lib/libcryptoauth.so ${TARGET_DIR}/usr/lib/python2.7/site-packages/cryptoauthlib/libcryptoauth.so
+ln -sf /usr/lib/libcryptoauth.so ${TARGET_DIR}/usr/lib/python3.8/site-packages/cryptoauthlib/libcryptoauth.so
