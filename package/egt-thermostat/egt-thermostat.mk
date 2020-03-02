6515adf59f60a29a893f29e17458a343253565f1
Joshua Henderson <joshua.henderson@microchip.com>
2020-03-02 13:41:36 -0700

    egt-thermostat: bump version
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- package/egt-thermostat/egt-thermostat.mk
+++ package/egt-thermostat/egt-thermostat.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EGT_THERMOSTAT_VERSION = 9feea90bbea3cc905030c6d85ee4e44495fdd002
+EGT_THERMOSTAT_VERSION = d7738e3bd0124643ad0c7f254349591246a1adf8
 EGT_THERMOSTAT_SITE = https://github.com/linux4sam/egt-thermostat.git
 EGT_THERMOSTAT_SITE_METHOD = git
 EGT_THERMOSTAT_GIT_SUBMODULES = YES
