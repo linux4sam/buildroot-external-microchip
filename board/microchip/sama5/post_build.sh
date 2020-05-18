c3d46c134daf264ff12d495e444d1cafcc4b352b
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-05-18 16:57:29 +0300

    sama5d2: fs-overlay: Add AWS GG HSI config files
    Add the config files required to setup Cryptoauthlib PKCS11 and
    Greengrass for all the sama5d2 boards.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- board/microchip/sama5/post_build.sh
+++ board/microchip/sama5/post_build.sh
@@ -5,3 +5,17 @@ echo "conf=${BR2_CONFIG}, hd=${HOST_DIR}, sd=${STAGING_DIR}, td=${TARGET_DIR}, b
 echo "Copying libcryptoauth.so to python2.7/site-packages"
 
 ln -sf /usr/lib/libcryptoauth.so ${TARGET_DIR}/usr/lib/python3.8/site-packages/cryptoauthlib/libcryptoauth.so
+
+# cryptoauthlib device configuration
+cp -p ${TARGET_DIR}/var/lib/cryptoauthlib/slot.conf.tmpl ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+if grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D2_XPLAINED=y$" ${BR2_CONFIG}; then
+	sed -i "s/interface = i2c.*/interface = i2c,0xC0,2/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D2_PTC_EK=y$" ${BR2_CONFIG}; then
+	sed -i "s/interface = i2c.*/interface = i2c,0xC0,1/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D2_ICP=y$" ${BR2_CONFIG}; then
+	sed -i "s/interface = i2c.*/interface = i2c,0xC0,1/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D27_SOM1_EK=y$" ${BR2_CONFIG}; then
+	sed -i "s/interface = i2c.*/interface = i2c,0xC0,0/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D27_WLSOM1_EK=y$" ${BR2_CONFIG}; then
+	sed -i "s/interface = i2c.*/interface = i2c,0x6A,0/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+fi
