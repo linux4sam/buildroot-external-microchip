2428179e081e833d361f875de4932efc9fa55ed3
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-05-26 11:31:49 +0000

    package: greengrass-core: Install package on target
    The package was not installed on the target, install it.
    
    Fixes: 70395eb1ab1a ("package: greengrass_core: Add AWS IoT Greengrass v1.10.1")
    Reported-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
    Suggested-by: Eugen Hristev <eugen.hristev@microchip.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- package/greengrass-core/greengrass-core.mk
+++ package/greengrass-core/greengrass-core.mk
@@ -4,8 +4,9 @@ GREENGRASS_CORE_SITE = https://d1onfpft10uf5o.cloudfront.net/greengrass-core/dow
 GREENGRASS_CORE_SOURCE = greengrass-linux-armv7l-${GREENGRASS_CORE_VERSION}.tar.gz
 GREENGRASS_CORE_LICENSE = Greengrass Core Software License Agreement
 GREENGRASS_CORE_LICENSE_FILES = LICENSE
+GREENGRASS_CORE_INSTALL_TARGET = YES
 
-define GREENGRASS_CORE_EXTRACT_CMDS
+define GREENGRASS_CORE_INSTALL_TARGET_CMDS
         $(call suitable-extractor, $(GREENGRASS_CORE_SOURCE)) \
 		$(GREENGRASS_CORE_DL_DIR)/$(GREENGRASS_CORE_SOURCE) | \
                 $(TAR) --strip-components=0 -C  $(TARGET_DIR)/ $(TAR_OPTIONS) -
