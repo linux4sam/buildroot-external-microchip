e6128fbd86159d156bc9417a2650637209f724c2
Eugen Hristev <eugen.hristev@microchip.com>
2020-05-26 17:38:43 +0300

    package/egt*: bump to 1.0
    Bump all egt packages to 1.0: launcher, benchmark, samples, thermostat, egt.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- package/egt/egt.mk
+++ package/egt/egt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-EGT_VERSION = 1.0-rc4
+EGT_VERSION = 1.0
 EGT_SITE = https://github.com/linux4sam/egt.git
 EGT_SITE_METHOD = git
 EGT_GIT_SUBMODULES = YES
