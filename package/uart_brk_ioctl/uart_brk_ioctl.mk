4e1037ed2e48494c73c4198d857e04c75e177a6a
Ajay Singh <ajay.kathat@microchip.com>
2020-09-25 09:25:48 +0000

    package/uart_brk_ioctl: Added package for UART BRK IOCTL
    Added uart_brk_ioctl package to handle the PS operation with wilc3000
    BLE.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>

---
+++ package/uart_brk_ioctl/uart_brk_ioctl.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# WILC3000 BLE uart break ioctl utility for PS
+#
+################################################################################
+
+UART_BRK_IOCTL_VERSION = 1.0
+UART_BRK_IOCTL_SITE = https://github.com/linux4wilc/uart_brk_ioctl.git
+UART_BRK_IOCTL_SITE_METHOD = git
+UART_BRK_LICENSE = GPL-2.0
+UART_BRK_LICENSE_FILES = LICENSE
+
+define UART_BRK_IOCTL_BUILD_CMDS
+	$(TARGET_CC) $(TARGET_CFLAGS) -Wall -std=gnu11 -g -D_REENTRANT -static $(@D)/uart_brk_ioctl.c -o $(@D)/uart_brk_ioctl
+endef
+
+define UART_BRK_IOCTL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/uart_brk_ioctl \
+		$(TARGET_DIR)/etc/
+endef
+
+$(eval $(generic-package))
