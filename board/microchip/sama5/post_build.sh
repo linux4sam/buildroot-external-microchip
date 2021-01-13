12aea4a9c8e81675fd1692e06f7855a6dec938af
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-01-13 19:04:12 +0200

    cryptoauthlib: Bump to v3.2.5
    Without configuring cryptoauthlib-v3.2.5 for PKCS11, one sees:
    pkcs11_init: PKCS #11 initialization error.
    warning: no token URL was provided for this operation; the available tokens are:
    
    https://github.com/MicrochipTech/cryptoauthlib/wiki/PKCS11-Linux-Setup
    fails to specify how to configure the library for PKCS11 linux.
    
    The config options were added by comparing the support between what
    was on pkcs11 branch and what is available in v3.2.5. This may be
    error-prone (not choosing the perfect configuration), but facing the
    lack of documentation, choose what seems right. Tested the AWS
    greengrass demo on sama5d27_wlsom1_ek, everything was fine.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Reviewed-by: Nicolas Ferre <nicolas.ferre@microchip.com>

--- board/microchip/sama5/post_build.sh
+++ board/microchip/sama5/post_build.sh
@@ -12,15 +12,15 @@ cp -p ${TARGET_DIR}/var/lib/cryptoauthlib/slot.conf.tmpl ${TARGET_DIR}/var/lib/c
 fi
 
 if grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D2_XPLAINED=y$" ${BR2_CONFIG}; then
-	sed -i "s/interface = i2c.*/interface = i2c,0xC0,2/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+	sed -i "s/interface = .*/interface = i2c,0xC0,2/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
 elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D2_PTC_EK=y$" ${BR2_CONFIG}; then
-	sed -i "s/interface = i2c.*/interface = i2c,0xC0,1/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+	sed -i "s/interface = .*/interface = i2c,0xC0,1/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
 elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D2_ICP=y$" ${BR2_CONFIG}; then
-	sed -i "s/interface = i2c.*/interface = i2c,0xC0,1/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+	sed -i "s/interface = .*/interface = i2c,0xC0,1/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
 elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D27_SOM1_EK=y$" ${BR2_CONFIG}; then
-	sed -i "s/interface = i2c.*/interface = i2c,0xC0,0/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+	sed -i "s/interface = .*/interface = i2c,0xC0,0/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
 elif grep -Eq "^BR2_PACKAGE_CRYPTOAUTHLIB_SAMA5D27_WLSOM1_EK=y$" ${BR2_CONFIG}; then
-	sed -i "s/interface = i2c.*/interface = i2c,0x6A,0/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
+	sed -i "s/interface = .*/interface = i2c,0x6A,0/" ${TARGET_DIR}/var/lib/cryptoauthlib/0.conf
 fi
 
 if grep -Eq "^BR2_PACKAGE_GREENGRASS_CORE=y$" ${BR2_CONFIG}; then
