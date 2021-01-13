b9b078cf426495d10f9942f617675280de759243
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-01-13 19:04:13 +0200

    greengrass-core: Bump to 1.11.0
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Reviewed-by: Nicolas Ferre <nicolas.ferre@microchip.com>

--- package/greengrass-core/greengrass-core.mk
+++ package/greengrass-core/greengrass-core.mk
@@ -1,5 +1,5 @@
 # AWS IoT Greengrass Core Software
-GREENGRASS_CORE_VERSION = 1.10.1
+GREENGRASS_CORE_VERSION = 1.11.0
 GREENGRASS_CORE_SITE = https://d1onfpft10uf5o.cloudfront.net/greengrass-core/downloads/${GREENGRASS_CORE_VERSION}
 GREENGRASS_CORE_SOURCE = greengrass-linux-armv7l-${GREENGRASS_CORE_VERSION}.tar.gz
 GREENGRASS_CORE_LICENSE = Greengrass Core Software License Agreement
