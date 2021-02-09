b6191d46ccd9b46c1c2da4362bc6e80d83ab416e
Eugen Hristev <eugen.hristev@microchip.com>
2021-02-09 19:46:06 +0200

    package/9bit: bump version and update for 2021.02
    According to buildroot upstream 2021.02 specs:
    
    Download: Rework the tarball creation logic (E.G. when a
    	package is fetched from a git/svn repo) to no longer require a
    	host-tar <= 1.29. To differ between the old and the new
    	format, the new tarballs have gained a -br1 suffix
    	(<pkg>-<version>-br1.tar.gz).
    
    The old style of getting a package was adding a '-br1' suffix to the
    tarball, which would make the hash check fail for this package.
    Update to a simpler github get method, and the checksum works fine,
    buildroot does not add -br1 to the tarball.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- package/9bit/9bit.mk
+++ package/9bit/9bit.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-9BIT_VERSION = df45b743003a78bb735eb3c2cb294f3cce3ae43a
-9BIT_SITE = https://github.com/linux4sam/9bit.git
+9BIT_VERSION = f4cd916c8c58300ea1cbb398f0b40fe43a70d6d6
+9BIT_SITE = $(call github,linux4sam,9bit,$(9BIT_VERSION))
 9BIT_LICENSE = MIT
 9BIT_LICENSE_FILES = COPYING
-9BIT_SITE_METHOD = git
 
 define 9BIT_BUILD_CMDS
 	$(TARGET_MAKE_ENV) CC="$(TARGET_CC)" LD="$(TARGET_LD)" $(MAKE) -C $(@D) all
