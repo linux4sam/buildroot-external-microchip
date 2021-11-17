2323611ca5376782f241f58747fb7f79aa2f2639
Ajay Singh <ajay.kathat@microchip.com>
2021-11-17 17:56:33 +0000

    package/wilc_firmware: bump to 15.6rc1
       Update to 15.6RC1
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>

--- package/wilc_firmware/wilc_firmware.mk
+++ package/wilc_firmware/wilc_firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WILC_FIRMWARE_VERSION = wilc_linux_15_5
+WILC_FIRMWARE_VERSION = wilc_linux_15_6rc1
 WILC_FIRMWARE_SITE = $(call github,linux4wilc,firmware,$(WILC_FIRMWARE_VERSION))
 WILC_FIRMWARE_LICENSE = Microchip firmware
 WILC_FIRMWARE_LICENSE_FILES = LICENSE.wilc_fw
