3b7a085f33cc5f69ba8347553c8088aa88d17e2a
Ludovic Desroches <ludovic.desroches@microchip.com>
2021-11-19 17:20:14 +0100

    package/egt-samples-contribution: new package
    New package for the EGT samples made by the community.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>

---
+++ package/egt-samples-contribution/egt-samples-contribution.mk
@@ -0,0 +1,19 @@
+###############################################################################
+#
+# egt sample applications from the community
+#
+###############################################################################
+
+EGT_SAMPLES_CONTRIBUTION_VERSION = 1.0-rc1
+EGT_SAMPLES_CONTRIBUTION_SITE = https://github.com/linux4sam/egt-samples-contribution.git
+EGT_SAMPLES_CONTRIBUTION_SITE_METHOD = git
+EGT_SAMPLES_CONTRIBUTION_LICENSE = Apache-2.0
+EGT_SAMPLES_CONTRIBUTION_INSTALL_TARGET = YES
+EGT_SAMPLES_CONTRIBUTION_DEPENDENCIES = egt
+
+ifeq ($(BR2_PACKAGE_EGT_SAMPLES_CONTRIBUTION_SLIDERB),y)
+EGT_SAMPLES_CONTRIBUTION_CONF_OPTS += -DEGT_SAMPLES_CONTRIBUTION_SLIDERB=TRUE
+endif
+
+$(eval $(cmake-package))
+
