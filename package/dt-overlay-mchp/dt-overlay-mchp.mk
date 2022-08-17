d88fc7de38858a41d991bfbb69bc3085b300d828
Jamie Gibbons <jamie.gibbons@microchip.com>
2022-08-17 14:37:09 +0100

    dt-overlay: add architecture selection and deploy .its
    Add conditional to detect if architecture is riscv or arm.
    Pass selected architecture to image creation.
    .its is needed to support the post script for the icicle kit
    configuration. Add .its to deploy along with the .itb.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>

--- package/dt-overlay-mchp/dt-overlay-mchp.mk
+++ package/dt-overlay-mchp/dt-overlay-mchp.mk
@@ -20,14 +20,20 @@ define DT_OVERLAY_MCHP_INSTALL_TARGET_CMDS
 			$(BINARIES_DIR))
 endef
 else
+ifeq ($(BR2_riscv),y)
+	dt_overlay_arch := riscv
+else
+	dt_overlay_arch := arm
+endif
 define DT_OVERLAY_MCHP_BUILD_CMDS
-	PATH="$(LINUX_DIR)/scripts/dtc:$(HOST_DIR)/bin:$(PATH)" $(MAKE) DTC="$(LINUX_DIR)/scripts/dtc/dtc" KERNEL_DIR="$(LINUX_DIR)" -C $(@D) $(BR2_PACKAGE_DT_OVERLAY_MCHP_PLATFORM).itb
+	ARCH=$(dt_overlay_arch) PATH="$(LINUX_DIR)/scripts/dtc:$(HOST_DIR)/bin:$(PATH)" $(MAKE) DTC="$(LINUX_DIR)/scripts/dtc/dtc" KERNEL_DIR="$(LINUX_DIR)" -C $(@D) $(BR2_PACKAGE_DT_OVERLAY_MCHP_PLATFORM).itb
 endef
 define DT_OVERLAY_MCHP_INSTALL_TARGET_CMDS
 	$(foreach f,$(notdir $(wildcard $(@D)/*/*.dtbo)),
 		$(INSTALL) -m 0644 -D $(@D)/$(BR2_PACKAGE_DT_OVERLAY_MCHP_PLATFORM)/$(f) \
 			$(BINARIES_DIR))
 	$(INSTALL) -m 0644 -D $(@D)/$(BR2_PACKAGE_DT_OVERLAY_MCHP_PLATFORM).itb $(BINARIES_DIR)/
+	$(INSTALL) -m 0644 -D $(@D)/$(BR2_PACKAGE_DT_OVERLAY_MCHP_PLATFORM).its $(BINARIES_DIR)/
 endef
 endif
 
