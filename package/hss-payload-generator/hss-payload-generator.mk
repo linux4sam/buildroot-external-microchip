62320d126a674b6494fbbf1c3d3590c0db746caa
Jamie Gibbons <jamie.gibbons@microchip.com>
2022-08-17 14:37:11 +0100

    package: add hart software service payload generator
    The Hart Software Service (HSS) package is not being built.
    Buildroot External uses the Hart Software Service's (HSS) payload
    generator tool. This tool creates a formatted payload image for
    the HSS zero-stage bootloader on PolarFire SoC, given a
    configuration file and a set of ELF binaries. The configuration
    file is used to map the ELF binaries or binary blobs to the
    individual application harts (U54s). Add the HSS payload generator
    package to support this.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>

---
+++ package/hss-payload-generator/hss-payload-generator.mk
@@ -0,0 +1,33 @@
+################################################################################
+ #
+ # Hart Software Services
+ #
+################################################################################
+HSS_PAYLOAD_GENERATOR_VERSION = 2021.11
+HSS_PAYLOAD_GENERATOR_SITE = $(call github,polarfire-soc,hart-software-services,$(HSS_PAYLOAD_GENERATOR_VERSION))
+HSS_PAYLOAD_GENERATOR_INSTALL_STAGING = NO
+HSS_PAYLOAD_GENERATOR_INSTALL_TARGET = YES
+HSS_PAYLOAD_GENERATOR_LICENSE = MIT
+HSS_PAYLOAD_GENERATOR_LICENSE_FILES = LICENSE.md
+
+ifeq ($(BR2_PACKAGE_MPFS_AMP_EXAMPLES),y)
+HOST_HSS_PAYLOAD_GENERATOR_DEPENDENCIES += uboot mpfs_amp_examples
+else
+HOST_HSS_PAYLOAD_GENERATOR_DEPENDENCIES += uboot
+endif
+
+define HOST_HSS_PAYLOAD_GENERATOR_BUILD_CMDS
+	$(MAKE) -C $(@D)/tools/hss-payload-generator
+
+	cp $(BR2_PACKAGE_HOST_HSS_PAYLOAD_GENERATOR_SRC) $(@D)/tools/hss-payload-generator/src.bin
+	( \
+		if [ "$(BR2_PACKAGE_ MPFS_AMP_EXAMPLES)" = "y" ]; then \
+			cp $(BINARIES_DIR)/mpfs-rpmsg-remote.elf $(@D)/tools/hss-payload-generator/amp.elf; \
+		fi; \
+		cd $(@D)/tools/hss-payload-generator; \
+		./hss-payload-generator -c $(BR2_PACKAGE_HOST_HSS_PAYLOAD_GENERATOR_CFG) -v $(BINARIES_DIR)/payload.bin; \
+	)
+
+endef
+
+$(eval $(host-generic-package))
