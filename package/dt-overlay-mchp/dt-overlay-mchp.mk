c095090b2ff135b2943a8c25e8ab301c7bc30469
Eugen Hristev <eugen.hristev@microchip.com>
2022-10-27 10:15:46 +0300

    package/dt-overlay-mchp: bump to linux4microchip-2022.10-rc2
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- package/dt-overlay-mchp/dt-overlay-mchp.mk
+++ package/dt-overlay-mchp/dt-overlay-mchp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DT_OVERLAY_MCHP_VERSION = linux4microchip-2022.10-rc1
+DT_OVERLAY_MCHP_VERSION = linux4microchip-2022.10-rc2
 DT_OVERLAY_MCHP_SITE = $(call github,linux4microchip,dt-overlay-mchp,$(DT_OVERLAY_MCHP_VERSION))
 DT_OVERLAY_MCHP_LICENSE = GPL-2.0 MIT
 DT_OVERLAY_MCHP_LICENSE_FILES = COPYING LICENSES/GPL-2.0 LICENSES/MIT
