2790fac1847f5f1082ae0668b8a52d72c8d9daa8
Eugen Hristev <eugen.hristev@microchip.com>
2022-11-02 17:24:16 +0200

    package/video-capture-at91: bump version
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- package/video-capture-at91/video-capture-at91.mk
+++ package/video-capture-at91/video-capture-at91.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIDEO_CAPTURE_AT91_VERSION = 9cbeed745ab98f1a847e349beb30ac16c0d1ca08
+VIDEO_CAPTURE_AT91_VERSION = 9bce302fb64fe7684d398a8effa8aeb87970310d
 VIDEO_CAPTURE_AT91_SITE = $(call github,linux4sam,video-capture-at91,$(VIDEO_CAPTURE_AT91_VERSION))
 VIDEO_CAPTURE_AT91_LICENSE = MIT
 VIDEO_CAPTURE_AT91_LICENSE_FILES = LICENSES/MIT
