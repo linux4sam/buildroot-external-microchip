bd85d0e5b3ae823fa40ddd525d04cff4149c80f5
Jamie Gibbons <jamie.gibbons@microchip.com>
2022-11-02 14:05:41 +0000

    configs: icicle: support additional overlay configs
    When working on development of adding dt-overlay configurations for
    the icicle kit, originally a change to the dt-overlay-mchp.mk had been made
    to make these configurations work (see v1/2). Upon further development, it
    was discovered that a typo in the dt-overlay Makefile for the dtbo object
    variables was the reason this problem.
    Upon fixing this error, it highlighted that a different structure of the images
    directory with the .dtbo files. Update the post image script to allow the same
    functionality as before with this change in folder structure reflected. This
    update allows additional configurations to be used correctly without the previous
    changes to the dt-overlay-mchp.mk (see v1/2).
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- board/microchip/icicle/post-image.sh
+++ board/microchip/icicle/post-image.sh
@@ -3,4 +3,6 @@
 cd ${BINARIES_DIR}
 mkdir dts
 cp -r microchip/ dts/
+mkdir mpfs_icicle
+mv *.dtbo mpfs_icicle/
 ${BUILD_DIR}/uboot-mpfs-uboot-2022.01/tools/mkimage -f mpfs_icicle.its mpfs_icicle.itb
