9444fe0c241ceb0b071549752f5be94cb5cf416d
Jamie Gibbons <jamie.gibbons@microchip.com>
2022-11-02 14:05:38 +0000

    mpfs_examples: update package to add examples
    Update package to newer release tag v2022.09 to add dt-overlays
    example and pdma example projects. This can be used to modify dt overlays
    from Linux user-space using configfs. Update keys with mpfs_ prefix.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Reviewed-by: Valentina Fernandez

--- package/mpfs_examples/mpfs_examples.mk
+++ package/mpfs_examples/mpfs_examples.mk
@@ -3,35 +3,35 @@
  # Microchip PolarFire SoC Examples
  #
 ################################################################################
-MPFS_EXAMPLES_VERSION = 91aa8b53f3a50c5dfd85a48feff36a5815fe5f49
+MPFS_EXAMPLES_VERSION = v2022.09
 MPFS_EXAMPLES_SITE = https://github.com/polarfire-soc/polarfire-soc-linux-examples.git
 MPFS_EXAMPLES_SITE_METHOD = git
 MPFS_EXAMPLES_LICENSE = MIT
 MPFS_EXAMPLES_LICENSE_FILES = LICENSE
 
 
-EXAMPLE_DIRS += amp can gpio system-services ethernet fpga-fabric-interfaces dma pdma
-EXAMPLE_FILES += amp/rpmsg-pingpong/rpmsg-pingpong amp/rpmsg-tty-example/rpmsg-tty can/uio-can-example gpio/gpiod-test gpio/gpio-event system-services/system-services-example system-services/signature-verification-demo fpga-fabric-interfaces/lsram/uio-lsram-read-write dma/uio-dma-interrupt pdma/pdma-ex
-EXAMPLE_TARGET_DIR = /opt/microchip/
+MPFS_EXAMPLE_DIRS += amp can gpio system-services ethernet fpga-fabric-interfaces dma dt-overlays pdma
+MPFS_EXAMPLE_FILES += amp/rpmsg-pingpong/rpmsg-pingpong amp/rpmsg-tty-example/rpmsg-tty can/uio-can-example gpio/gpiod-test gpio/gpio-event system-services/system-services-example system-services/signature-verification-demo fpga-fabric-interfaces/lsram/uio-lsram-read-write dma/uio-dma-interrupt pdma/pdma-ex
+MPFS_EXAMPLE_TARGET_DIR = /opt/microchip/
 
 define MPFS_EXAMPLES_INSTALL_DIRS
-	$(foreach d,$(EXAMPLE_DIRS), \
-		rm -rf $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)$(d); \
-		cp -a $(@D)/$(d) $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)$(d)$(sep))
+	$(foreach d,$(MPFS_EXAMPLE_DIRS), \
+		rm -rf $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)$(d); \
+		cp -a $(@D)/$(d) $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)$(d)$(sep))
 endef
 
 define MPFS_EXAMPLES_INSTALL_TARGET_CMDS
-        $(INSTALL) -d $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)
-	$(foreach d,$(EXAMPLE_DIRS), \
-		rm -rf $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)$(d); \
-		cp -a $(@D)/$(d) $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)$(d)$(sep))
+        $(INSTALL) -d $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)
+	$(foreach d,$(MPFS_EXAMPLE_DIRS), \
+		rm -rf $(TARGET_DIR)$(MPFS_XAMPLE_TARGET_DIR)$(d); \
+		cp -a $(@D)/$(d) $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)$(d)$(sep))
 
-        echo $(EXAMPLE_FILES)
-	$(foreach example_file,$(EXAMPLE_FILES), \
+        echo $(MPFS_EXAMPLE_FILES)
+	$(foreach example_file,$(MPFS_EXAMPLE_FILES), \
 		$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$(dir $(example_file)) $(notdir $(example_file)) CC=$(TARGET_CC); \
-                $(INSTALL) -D -m 775 $(@D)/$(example_file) $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)/$(dir $(example_file));)
+                $(INSTALL) -D -m 775 $(@D)/$(example_file) $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)/$(dir $(example_file));)
 
-	ln -sf $(EXAMPLE_TARGET_DIR)/ethernet/iio-http-server  $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)/iiohttpserver
+	ln -sf $(MPFS_EXAMPLE_TARGET_DIR)/ethernet/iio-http-server  $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)/iiohttpserver
 
 endef
 
@@ -45,9 +45,9 @@ define MPFS_EXAMPLES_INSTALL_INIT_SYSV
 $(INSTALL) -D -m 775 $(@D)/ethernet/iio-http-server/collection/collectdiio.busybox \
         $(TARGET_DIR)/etc/init.d/collectdiio
 # busy box init requires script renames
-mv $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{sh,systemd}
-mv $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{busybox,sh}
-chmod +x $(TARGET_DIR)$(EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.sh
+mv $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{sh,systemd}
+mv $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{busybox,sh}
+chmod +x $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.sh
 
 endef
 
