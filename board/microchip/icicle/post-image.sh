46188cea9cb316bab7fabc88705e86ae78c08bf9
Valentina Fernandez <valentina.fernandezalanis@microchip.com>
2022-11-18 12:15:00 +0000

    board: icicle{,amp}: use gzip compressed linux image
    Update the icicle and icicle_amp post scripts to use a compressed
    kernel image for generating the itb using mkimage. Also, use the mkimage
    binary provided in the HOST_DIR.
    
    Signed-off-by: Valentina Fernandez <valentina.fernandezalanis@microchip.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>

--- board/microchip/icicle/post-image.sh
+++ board/microchip/icicle/post-image.sh
@@ -5,4 +5,5 @@ mkdir dts
 cp -r microchip/ dts/
 mkdir mpfs_icicle
 mv *.dtbo mpfs_icicle/
-${BUILD_DIR}/uboot-mpfs-uboot-2022.01/tools/mkimage -f mpfs_icicle.its mpfs_icicle.itb
+gzip -9 Image -c > Image.gz
+$HOST_DIR/bin/mkimage -f mpfs_icicle.its mpfs_icicle.itb
--- board/microchip/icicle_amp/post-image.sh
+++ board/microchip/icicle_amp/post-image.sh
@@ -5,4 +5,5 @@ mkdir dts
 cp -r microchip/ dts/
 mkdir mpfs_icicle_amp
 cp *.dtbo mpfs_icicle_amp/
+gzip -9 Image -c > Image.gz
 $HOST_DIR/bin/mkimage -f mpfs_icicle_amp.its mpfs_icicle_amp.itb
