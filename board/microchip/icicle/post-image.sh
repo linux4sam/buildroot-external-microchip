410bce7e632e11173413c3b282784536e11a55ca
Valentina Fernandez <valentina.fernandezalanis@microchip.com>
2023-01-03 16:09:20 +0000

    board: icicle{,amp}: update post image script to include genimage cmd
    Update the post image script config (BR2_ROOTFS_POST_IMAGE_SCRIPT) on
    the icicle kit defconfigs to do the image generation process (genimg)
    as part of the post-image.sh script instead of two independent scripts.
    
    Also, add -p argument to mkdir to avoid warnings if directory already
    exists.
    
    Signed-off-by: Valentina Fernandez <valentina.fernandezalanis@microchip.com>

--- board/microchip/icicle/post-image.sh
+++ board/microchip/icicle/post-image.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+BASE_DIR="$(pwd)"
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="$2"
+
 cd ${BINARIES_DIR}
-mkdir dts
+mkdir -p dts
 cp -r microchip/ dts/
-mkdir mpfs_icicle
+mkdir -p mpfs_icicle
 mv *.dtbo mpfs_icicle/
 gzip -9 Image -c > Image.gz
 $HOST_DIR/bin/mkimage -f mpfs_icicle.its mpfs_icicle.itb
+${BASE_DIR}/support/scripts/genimage.sh -c ${GENIMAGE_CFG}
--- board/microchip/icicle_amp/post-image.sh
+++ board/microchip/icicle_amp/post-image.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+BASE_DIR="$(pwd)"
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="$2"
+
 cd ${BINARIES_DIR}
-mkdir dts
+mkdir -p dts
 cp -r microchip/ dts/
-mkdir mpfs_icicle_amp
+mkdir -p mpfs_icicle_amp
 cp *.dtbo mpfs_icicle_amp/
 gzip -9 Image -c > Image.gz
 $HOST_DIR/bin/mkimage -f mpfs_icicle_amp.its mpfs_icicle_amp.itb
+${BASE_DIR}/support/scripts/genimage.sh -c ${GENIMAGE_CFG}
