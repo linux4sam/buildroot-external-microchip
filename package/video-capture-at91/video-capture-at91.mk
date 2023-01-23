71a08886d74e04f32e2454551b546e3fa4dbb777
Hari Prasath <Hari.PrasathGE@microchip.com>
2023-01-23 11:34:02 +0530

    package/video-capture-at91: bump version
    Signed-off-by: Hari Prasath <Hari.PrasathGE@microchip.com>

--- package/video-capture-at91/video-capture-at91.mk
+++ package/video-capture-at91/video-capture-at91.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIDEO_CAPTURE_AT91_VERSION = 9bce302fb64fe7684d398a8effa8aeb87970310d
+VIDEO_CAPTURE_AT91_VERSION = 882ede15dd4aec22728b1883f253ade3f25a65c6
 VIDEO_CAPTURE_AT91_SITE = $(call github,linux4sam,video-capture-at91,$(VIDEO_CAPTURE_AT91_VERSION))
 VIDEO_CAPTURE_AT91_LICENSE = MIT
 VIDEO_CAPTURE_AT91_LICENSE_FILES = LICENSES/MIT
