d79ca75fa993d2b344b3d1f534a6476cd1da7d8d
Jamie Gibbons <jamie.gibbons@microchip.com>
2023-04-14 17:13:18 +0100

    mpfs_icicle: add overlays for use at runtime
    Buildroot currently only assembles the overlays to the FIT image and not
    to the file system. This causes the dt-overlay-mchp example to not
    function in Buildroot. Add the overlays to the /boot directory of the
    root file system.
    The dt-overlay-mchp example uses the /sys directory to modify the device
    tree. Mount /sys to avoid permission error when running this script.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- board/microchip/icicle/post-build.sh
+++ board/microchip/icicle/post-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-cp ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle/motd ${TARGET_DIR}/etc/motd
+if ! [ -d ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle/rootfs-overlay/boot ]; then
+	mkdir ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle/rootfs-overlay/boot
+fi
+if [ -d ${BINARIES_DIR}/mpfs_icicle ]; then
+	cp ${BINARIES_DIR}/mpfs_icicle/*.dtbo ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle/rootfs-overlay/boot
+else
+	cp ${BINARIES_DIR}/*.dtbo ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle/rootfs-overlay/boot
+fi
--- board/microchip/icicle_amp/post-build.sh
+++ board/microchip/icicle_amp/post-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-cp ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle_amp/motd ${TARGET_DIR}/etc/motd
+if ! [ -d ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle_amp/rootfs-overlay/boot ]; then
+	mkdir ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle_amp/rootfs-overlay/boot
+fi
+if [ -d ${BINARIES_DIR}/mpfs_icicle_amp ]; then
+	cp ${BINARIES_DIR}/mpfs_icicle_amp/*.dtbo ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle_amp/rootfs-overlay/boot
+else
+	cp ${BINARIES_DIR}/*.dtbo ${BR2_EXTERNAL_MCHP_PATH}/board/microchip/icicle_amp/rootfs-overlay/boot
+fi
