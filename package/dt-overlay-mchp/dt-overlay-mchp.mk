d1f0b5794aca986e268f0155e9377fe8d9a2229b
Sayed Nayab Basha <nayabbasha.sayed@microchip.com>
2023-04-17 19:54:17 +0530

    package/dt-overlay-mchp: bump to linux4microchip-2023.04-rc1
    Bump dt-overlay-mchp version from linux4microchip+fpga-2023.02 to
    linux4microchip-2023.04-rc1
    
    Tested-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Signed-off-by: Sayed Nayab Basha <nayabbasha.sayed@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- package/dt-overlay-mchp/dt-overlay-mchp.mk
+++ package/dt-overlay-mchp/dt-overlay-mchp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DT_OVERLAY_MCHP_VERSION = linux4microchip+fpga-2023.02
+DT_OVERLAY_MCHP_VERSION = linux4microchip-2023.04-rc1
 DT_OVERLAY_MCHP_SITE = $(call github,linux4microchip,dt-overlay-mchp,$(DT_OVERLAY_MCHP_VERSION))
 DT_OVERLAY_MCHP_LICENSE = GPL-2.0 MIT
 DT_OVERLAY_MCHP_LICENSE_FILES = COPYING LICENSES/GPL-2.0 LICENSES/MIT
