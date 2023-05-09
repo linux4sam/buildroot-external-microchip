5aa1d132da1d45772f8befd5c9ac7e487158ffe3
Claudiu Beznea <claudiu.beznea@microchip.com>
2023-05-09 12:28:58 +0300

    package/video-capture-at91: bump version
    Bump version to benefit of kernel backward compatibility support.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- package/video-capture-at91/video-capture-at91.mk
+++ package/video-capture-at91/video-capture-at91.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIDEO_CAPTURE_AT91_VERSION = 9bce302fb64fe7684d398a8effa8aeb87970310d
+VIDEO_CAPTURE_AT91_VERSION = 7e3a0f7c37c1895f06b498caa5074fed4ab203d6
 VIDEO_CAPTURE_AT91_SITE = $(call github,linux4sam,video-capture-at91,$(VIDEO_CAPTURE_AT91_VERSION))
 VIDEO_CAPTURE_AT91_LICENSE = MIT
 VIDEO_CAPTURE_AT91_LICENSE_FILES = LICENSES/MIT
