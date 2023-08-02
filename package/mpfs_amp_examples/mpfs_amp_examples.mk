a781c1c177d4828db2f31d9cd2095684836c0e94
Jamie Gibbons <jamie.gibbons@microchip.com>
2023-08-02 10:15:57 +0100

    package/mpfs_amp_examples: cleanup
    Cleanup package makefile to align with proper Buildroot package styling
    rules.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- package/mpfs_amp_examples/mpfs_amp_examples.mk
+++ package/mpfs_amp_examples/mpfs_amp_examples.mk
@@ -1,11 +1,10 @@
 ################################################################################
- #
- # Microchip AMP Examples
- #
+#
+# Microchip PolarFire SoC AMP Examples
+#
 ################################################################################
-MPFS_AMP_EXAMPLES_VERSION = v2023.06
-MPFS_AMP_EXAMPLES_SITE = https://github.com/polarfire-soc/polarfire-soc-amp-examples.git
-MPFS_AMP_EXAMPLES_SITE_METHOD = git
+MPFS_AMP_EXAMPLES_VERSION = 2023.06
+MPFS_AMP_EXAMPLES_SITE = $(call github,polarfire-soc,polarfire-soc-amp-examples,v$(MPFS_AMP_EXAMPLES_VERSION))
 MPFS_AMP_EXAMPLES_INSTALL_TARGET = NO
 MPFS_AMP_EXAMPLES_INSTALL_IMAGES = YES
 MPFS_AMP_EXAMPLES_LICENSE = MIT
@@ -16,8 +15,10 @@ MPFS_AMP_EXAMPLES_TARGET_DIR = /lib/firmware
 
 define MPFS_AMP_EXAMPLES_BUILD_CMDS
 	$(foreach project,$(MPFS_AMP_EXAMPLES_DIRS), \
-		$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$(project) CROSS_COMPILE=$(TARGET_CROSS) REMOTE=1 REMOTEPROC=1 EXT_CFLAGS='-DMPFS_HAL_FIRST_HART=4 -DMPFS_HAL_LAST_HART=4'; \
-		$(INSTALL) -D -m 775 $(@D)/$(project)/Remote-Default/mpfs-rpmsg-remote.elf $(TARGET_DIR)$(MPFS_AMP_EXAMPLES_TARGET_DIR)/rproc-miv-rproc-fw;
+		$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/$(project) CROSS_COMPILE=$(TARGET_CROSS) \
+			REMOTE=1 REMOTEPROC=1 EXT_CFLAGS='-DMPFS_HAL_FIRST_HART=4 -DMPFS_HAL_LAST_HART=4'; \
+		$(INSTALL) -D -m 775 $(@D)/$(project)/Remote-Default/mpfs-rpmsg-remote.elf \
+			$(TARGET_DIR)$(MPFS_AMP_EXAMPLES_TARGET_DIR)/rproc-miv-rproc-fw;
 		cp $(@D)/$(project)/Remote-Default/mpfs-rpmsg-remote.elf $(BINARIES_DIR);)
 endef
 
