1234bce554c80a895d4385d9eeaed5c326466abb
Conor Dooley <conor.dooley@microchip.com>
2023-10-25 14:48:22 +0100

    Revert "packages/plplot: Use secure connections to download package"
    This reverts commit 4df6f098d7def4ee84de980bbd6983138ffe5949.
    Nayab reported that this commit broke downloading the tarball of the
    package, as the tarball is not present at the https:/ version of the
    URI. Revert the change until a suitable https URI can be found.
    
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- package/plplot/plplot.mk
+++ package/plplot/plplot.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PLPLOT_VERSION = plplot-5.15.0
-PLPLOT_SITE = https://git.code.sf.net/p/plplot/plplot
+PLPLOT_SITE = git://git.code.sf.net/p/plplot/plplot
 PLPLOT_LICENSE = GPL
 PLPLOT_LICENSE_FILES = Copyright
 PLPLOT_INSTALL_STAGING = YES
