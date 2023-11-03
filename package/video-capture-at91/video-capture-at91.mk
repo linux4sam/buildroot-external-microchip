fe0c781bc10042d896e56e9a9ff148e9e1a1c068
Nayab Sayed <nayabbasha.sayed@microchip.com>
2023-11-03 16:40:08 +0530

    package/video-capture-at91: bump version to dc8796111b4e64e2ca55190ae0118986f25f73ae
    This version adds imx219 image sensor related changes for sam9x75.
    
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Acked-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>
    Signed-off-by: Cristian Birsan <cristian.birsan@microchip.com>

--- package/video-capture-at91/video-capture-at91.mk
+++ package/video-capture-at91/video-capture-at91.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIDEO_CAPTURE_AT91_VERSION = 7e3a0f7c37c1895f06b498caa5074fed4ab203d6
+VIDEO_CAPTURE_AT91_VERSION = dc8796111b4e64e2ca55190ae0118986f25f73ae
 VIDEO_CAPTURE_AT91_SITE = $(call github,linux4sam,video-capture-at91,$(VIDEO_CAPTURE_AT91_VERSION))
 VIDEO_CAPTURE_AT91_LICENSE = MIT
 VIDEO_CAPTURE_AT91_LICENSE_FILES = LICENSES/MIT
