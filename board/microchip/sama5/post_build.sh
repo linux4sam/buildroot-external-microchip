4890bd613fd0c4e312be18f4c342d2fa2c6b76f3
Nayab Sayed <nayabbasha.sayed@microchip.com>
2023-11-17 14:30:18 +0530

    configs: bump optee os to version 4.0.0
    - Bumps optee os version to release 4.0.0.
    - Update source uri to point to open source OP-TEE repo
    - Add tee-supplicant systemd service file
    
    Co-developed-by: Dharma Balasubiramani <dharma.b@microchip.com>
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Acked-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- board/microchip/sama5/post_build.sh
+++ board/microchip/sama5/post_build.sh
@@ -55,3 +55,10 @@ if grep -Eq "^BR2_PACKAGE_SYSTEMD=y$" ${BR2_CONFIG}; then
 	rm -rf ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/wpa_supplicant.service
 	rm -rf ${TARGET_DIR}/usr/lib/systemd/system/wpa_supplicant.service
 fi
+if grep -Eq "^BR2_TARGET_OPTEE_OS=y$" ${BR2_CONFIG}; then
+	cp ${BR2_EXTERNAL}/package/optee-client/tee-supplicant.service \
+	${TARGET_DIR}/usr/lib/systemd/system/tee-supplicant.service
+	mkdir -p ${TARGET_DIR}/etc/systemd/system/basic.target.wants
+	ln -sf /usr/lib/systemd/system/tee-supplicant.service \
+	${TARGET_DIR}/etc/systemd/system/basic.target.wants/tee-supplicant.service
+fi
