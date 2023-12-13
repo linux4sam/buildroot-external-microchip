6144b963a26b9190e9a28df36233ab0ebc9463c5
Nayab Sayed <nayabbasha.sayed@microchip.com>
2023-12-13 14:30:36 +0530

    cryptoauthlib: bump to version 3.7.0
    Bump cryptoauthlib to the latest public release 3.7.0. And update the
    patch accordingly.
    
    For a list of implemented features, bug fixes and API changes in this
    release, please refer:
    https://github.com/MicrochipTech/cryptoauthlib/blob/main/release_notes.md
    
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Acked-by: Dharma Balasubiramani <dharma.b@microchip.com>
    Signed-off-by: Cristian Birsan <cristian.birsan@microchip.com>

--- package/cryptoauthlib/cryptoauthlib.mk
+++ package/cryptoauthlib/cryptoauthlib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CRYPTOAUTHLIB_VERSION = v3.5.1
+CRYPTOAUTHLIB_VERSION = v3.7.0
 CRYPTOAUTHLIB_SITE = $(call github,MicrochipTech,cryptoauthlib,$(CRYPTOAUTHLIB_VERSION))
 CRYPTOAUTHLIB_INSTALL_STAGING = YES
 CRYPTOAUTHLIB_LICENSE = LGPL-2.1
