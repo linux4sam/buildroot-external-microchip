7b20891cae54ac79829af19c32f26b8180d57de2
Valentina Fernandez <valentina.fernandezalanis@microchip.com>
2024-02-14 14:22:45 +0000

    boards: icicle: add boot script and fitImage to UBI filesystem
    Copy the U-Boot boot script and fitImage to the /boot directory in the
    UBI filesystem for icicle_nand_defconfig and icicle_nor_defconfig.
    This is particularly useful for the QSPI nand and nor images,
    since we can now use distro boot to automatically load a boot script
    from a UBI filesystem instead of using a custom QSPI boot target.
    
    Signed-off-by: Valentina Fernandez <valentina.fernandezalanis@microchip.com>
    Tested-by: Prajna Rajendra Kumar <prajna.rajendrakumar@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

---
+++ board/microchip/icicle/post-build-qspi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if ! [ -d ${TARGET_DIR}/boot ]; then
+	mkdir ${TARGET_DIR}/boot
+fi
+
+cp ${BINARIES_DIR}/boot.scr ${TARGET_DIR}/boot/boot.scr
+cp ${BINARIES_DIR}/mpfs_icicle.itb ${TARGET_DIR}/boot
