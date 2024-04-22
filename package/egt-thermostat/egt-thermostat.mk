3d06fa63df788bbde27cd3568335127788b31bac
Ludovic Desroches <ludovic.desroches@microchip.com>
2024-04-22 16:05:33 +0200

    package/egt-thermostat: bump version
    Bump egt-thermostat to 1.7-rc1 for v2024.04 and remove the dependency on
    sqlite and the database generation as database support within
    egt-thermostat is no longer enable by default.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- package/egt-thermostat/egt-thermostat.mk
+++ package/egt-thermostat/egt-thermostat.mk
@@ -4,24 +4,17 @@
 #
 ################################################################################
 
-EGT_THERMOSTAT_VERSION = 1.6
+EGT_THERMOSTAT_VERSION = 1.7-rc1
 EGT_THERMOSTAT_SITE = https://github.com/linux4sam/egt-thermostat.git
 EGT_THERMOSTAT_SITE_METHOD = git
 EGT_THERMOSTAT_GIT_SUBMODULES = YES
 EGT_THERMOSTAT_LICENSE = Apache-2.0
 EGT_THERMOSTAT_INSTALL_TARGET = YES
-EGT_THERMOSTAT_DEPENDENCIES = egt host-sqlite
+EGT_THERMOSTAT_DEPENDENCIES = egt
 
 define EGT_THERMOSTAT_RUN_AUTOGEN
         cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
 endef
 EGT_THERMOSTAT_POST_PATCH_HOOKS += EGT_THERMOSTAT_RUN_AUTOGEN
-
-define EGT_THERMOSTAT_INSTALL_INIT
-        cd $(@D) && PATH=$(BR_PATH) sqlite3 thermostat.db < thermostat.sql
-	$(INSTALL) -m 0755 -D $(EGT_THERMOSTAT_BUILDDIR)/thermostat.db $(TARGET_DIR)/usr/share/egt/thermostat/thermostat.db
-endef
-
-EGT_THERMOSTAT_POST_INSTALL_TARGET_HOOKS += EGT_THERMOSTAT_INSTALL_INIT
 
 $(eval $(autotools-package))
