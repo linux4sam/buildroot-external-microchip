f6782c0a1cc3e0ce48400b5df5eea8b04a746c38
Jamie Gibbons <jamie.gibbons@microchip.com>
2024-07-30 10:17:05 +0100

    icicle_amp: fix post build script
    When the device tree overlays were updated to configure AMP the
    mpfs_icicle_amp directory became redundant and is now mpfs_icicle for
    all icicle configurations. Therefore, update post build script to
    reflect this.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- board/microchip/icicle_amp/post-build.sh
+++ board/microchip/icicle_amp/post-build.sh
@@ -3,8 +3,8 @@
 if ! [ -d ${TARGET_DIR}/boot ]; then
 	mkdir ${TARGET_DIR}/boot
 fi
-if [ -d ${BINARIES_DIR}/mpfs_icicle_amp ]; then
-	cp ${BINARIES_DIR}/mpfs_icicle_amp/*.dtbo ${TARGET_DIR}/boot
+if [ -d ${BINARIES_DIR}/mpfs_icicle ]; then
+	cp ${BINARIES_DIR}/mpfs_icicle/*.dtbo ${TARGET_DIR}/boot
 else
 	cp ${BINARIES_DIR}/*.dtbo ${TARGET_DIR}/boot
 fi
