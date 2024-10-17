b9df0766d54d023419723b89774ca1849d4febd0
Nayab Sayed <nayabbasha.sayed@microchip.com>
2024-10-17 14:44:22 +0530

    board: sam9x75_curiosity: fs-overlay: add Start_AP.sh demo script
    Start_AP.sh script configures and brings up target wireless interface into
    AP mode when run from within the board.
    
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Reviewed-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

---
+++ board/microchip/sam9x75_curiosity/fs-overlay/root/Start_AP.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+echo "---------------------------------------------------"
+echo " #     #                                           "
+echo " #  #  # ###### #       ####   ####  #    # ###### "
+echo " #  #  # #      #      #    # #    # ##  ## #      "
+echo " #  #  # #####  #      #      #    # # ## # #####  "
+echo " #  #  # #      #      #      #    # #    # #      "
+echo " #  #  # #      #      #    # #    # #    # #      "
+echo "  ## ##  ###### ######  ####   ####  #    # ###### "
+echo "---------------------------------------------------"
+echo "    #    ######     ######                         "
+echo "   # #   #     #    #     # ###### #    #  ####    "
+echo "  #   #  #     #    #     # #      ##  ## #    #   "
+echo " #     # ######     #     # #####  # ## # #    #   "
+echo " ####### #          #     # #      #    # #    #   "
+echo " #     # #          #     # #      #    # #    #   "
+echo " #     # #          ######  ###### #    #  ####    "
+echo "---------------------------------------------------"
+
+if lsmod | grep -q "wilc_sdio" ; then
+    echo "1.############## WILC-SDIO module is available ##############"
+else
+    echo "1. Inserting the wilc-sdio module"
+    modprobe wilc-sdio
+    if lsmod | grep -q "wilc_sdio";  then
+        echo "WILC-SDIO module insterted successfully"
+    else
+        echo "WILC-SDIO module insert failed"
+    exit 0
+    fi
+fi
+
+echo "3.############# Stopping wpa_supplicant service if any ####"
+systemctl stop wpa_supplicant.service
+
+echo "4.############# Loading network configuration  #######"
+cp /usr/lib/systemd/system/hostapd@.service.example /etc/systemd/system/hostapd@.service
+cp /usr/lib/systemd/network/80-wifi-softap.network.example /etc/systemd/network/wlan0.network
+networkctl reload
+
+echo "5.############## Starting the Host AP deamon ##############"
+systemctl start hostapd@open.service
+
+if ps | grep -q "hostapd" ;
+then
+    echo "hostapd process has started successfully"
+else
+    echo "hostapd has failed to start"
+    exit 0
+fi
+nohup python3 -m http.server 80 -d /var/www/html &
+echo "Now, The device comes up as an Access Point(AP) and host a webpage to provision"
+echo "WiFi station interface"
+echo "\n"
+echo "Use a Phone/Laptop and connect to the 'wilc1000_SoftAP' WiFi AP"
+echo "Using the web browser open http://192.168.1.1"
+echo "---------------------------------------------------"
+echo "---------------------------------------------------"
