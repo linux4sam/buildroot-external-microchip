a014998e86462e65a7fe83d79ea01ac4f8bdbdbd
Nayab Sayed <nayabbasha.sayed@microchip.com>
2024-10-29 14:17:54 +0530

    package/ble-bluez-hci-apps: add new package
    This package contains ble-uart-bluez and dfu-bluez apps to communicate with
    RNBD451 HCI controller.
    
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Reviewed-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

---
+++ package/ble-bluez-hci-apps/ble_bluez_hci_apps.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# bluez ble sw apps: ble-uart-bluez and dfu-bluez
+#
+################################################################################
+
+BLE_BLUEZ_HCI_APPS_SITE = https://github.com/linux4microchip/ble_bluez_hci_apps.git
+BLE_BLUEZ_HCI_APPS_VERSION = 27aee9b024985366ea0c9e2a77ce02113cc46252
+BLE_BLUEZ_HCI_APPS_SITE_METHOD = git
+BLE_BLUEZ_HCI_APPS_LICENSE = Microchip firmware
+
+BLE_BLUEZ_HCI_APPS_DEPENDENCIES = bluez5_utils
+
+define BLE_BLUEZ_HCI_APPS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/apps/ble_uart_app/ble-uart-bluez $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/apps/dfu_app/dfu-bluez $(TARGET_DIR)/usr/bin/
+endef
+
+$(eval $(cmake-package))
