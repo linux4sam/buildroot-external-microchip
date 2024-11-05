a9c74b587ac8439f159977830eb1338d5ef09ba4
Balakrishnan Sambath <balakrishnan.s@microchip.com>
2024-11-05 15:22:55 +0530

    package/mchpcam-apps: add new package
    Add mchpcam applications package, include mchpcam-still demo application
    for libcamera-mchp.
    
    Signed-off-by: Balakrishnan Sambath <balakrishnan.s@microchip.com>
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

---
+++ package/mchpcam-apps/mchpcam-apps.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# mchpcam-apps
+#
+################################################################################
+MCHPCAM_APPS_VERSION = c018384960694ffc9f13f6a674e75cb6624ae489
+MCHPCAM_APPS_SITE = $(call github,linux4microchip,libcamera-mchp,$(MCHPCAM_APPS_VERSION))
+MCHPCAM_APPS_LICENSE = \
+    GPL-2.0+ (utils), \
+    CC0-1.0 (meson build system)
+MCHPCAM_APPS_LICENSE_FILES = \
+    LICENSES/GPL-2.0-or-later.txt \
+    LICENSES/GPL-2.0-only.txt \
+    LICENSES/CC0-1.0.txt
+MCHPCAM_APPS_DEPENDENCIES = libcamera-mchp
+MCHPCAM_APPS_CONF_OPTS = \
+	-Dpipelines=microchip-isc \
+	-Dv4l2=false \
+	-Ddocumentation=disabled \
+	-Dgstreamer=disabled \
+	-Dpycamera=disabled \
+	-Dmchpcam=enabled
+
+define MCHPCAM_APPS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/build/src/apps/mchpcam/mchpcam-still \
+		$(TARGET_DIR)/usr/bin/mchpcam-still
+endef
+
+$(eval $(meson-package))
+
