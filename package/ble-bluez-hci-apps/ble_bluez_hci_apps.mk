e8df13487b68f85ef238c3f58189f2a0e1ba2b64
Nayab Sayed <nayabbasha.sayed@microchip.com>
2024-11-08 14:33:30 +0530

    package/ble_bluez_hci_apps: prevent build failure
    ble_bluez_hci_apps needs the header files from package
    bluez5_utils-headers. Select the dependency to prevent package build
    failure.
    
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Reviewed-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- package/ble-bluez-hci-apps/ble_bluez_hci_apps.mk
+++ package/ble-bluez-hci-apps/ble_bluez_hci_apps.mk
@@ -9,7 +9,7 @@ BLE_BLUEZ_HCI_APPS_VERSION = 27aee9b024985366ea0c9e2a77ce02113cc46252
 BLE_BLUEZ_HCI_APPS_SITE_METHOD = git
 BLE_BLUEZ_HCI_APPS_LICENSE = Microchip firmware
 
-BLE_BLUEZ_HCI_APPS_DEPENDENCIES = bluez5_utils
+BLE_BLUEZ_HCI_APPS_DEPENDENCIES = bluez5_utils bluez5_utils-headers
 
 define BLE_BLUEZ_HCI_APPS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/apps/ble_uart_app/ble-uart-bluez $(TARGET_DIR)/usr/bin/
