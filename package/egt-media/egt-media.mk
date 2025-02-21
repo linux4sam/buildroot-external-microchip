b258849c54e2430f6db9d3bb7173eee36785e2bc
Nayab Sayed <nayabbasha.sayed@microchip.com>
2025-02-21 14:12:46 +0530

    package/egt-media: update package download source
    Update the makefile to fetch from the github repo instead of from the
    linux4sam file server.
    
    Signed-off-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Reviewed-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- package/egt-media/egt-media.mk
+++ package/egt-media/egt-media.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 EGT_MEDIA_VERSION = 1.3
-EGT_MEDIA_SITE = https://files.linux4sam.org/pub/demo/media
-EGT_MEDIA_SOURCE = egt-media-${EGT_MEDIA_VERSION}.tar.gz
+EGT_MEDIA_SITE = https://github.com/linux4sam/egt-media.git
+EGT_MEDIA_SITE_METHOD = git
 EGT_MEDIA_LICENSE = Apache-2.0
 EGT_MEDIA_INSTALL_TARGET = YES
 EGT_MEDIA_INSTALL_STAGING = YES
@@ -15,7 +15,7 @@ define EGT_MEDIA_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/egt/examples
         $(call suitable-extractor, $(EGT_MEDIA_SOURCE)) \
 		$(EGT_MEDIA_DL_DIR)/$(EGT_MEDIA_SOURCE) | \
-                $(TAR) --strip-components=1 -C  $(TARGET_DIR)/usr/share/egt/examples $(TAR_OPTIONS) -
+                $(TAR) --strip-components=1 -C  $(TARGET_DIR)/usr/share/egt/ $(TAR_OPTIONS) -
 endef
 
 $(eval $(generic-package))
