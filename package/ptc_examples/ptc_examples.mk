69fc5f90ec693ef478b207aeab0bc637509b69d4
Ludovic Desroches <ludovic.desroches@microchip.com>
2025-11-03 15:51:54 +0100

    package/ptc_examples: bump to 1.4.0-rc1, migrate to libgpiod 2.x & CMake
    The 1.4 version of ptc_examples switches from libgpiod 1.x to 2.x and
    adopts CMake as the build system.
    
    Buildroot ships separate packages for libgpiod 1.x and 2.x, so update
    defconfigs to select the 2.x package.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
    Reviewed-by: Nayab Sayed <nayabbasha.sayed@microchip.com>

--- package/ptc_examples/ptc_examples.mk
+++ package/ptc_examples/ptc_examples.mk
@@ -4,26 +4,26 @@
 #
 ################################################################################
 
-PTC_EXAMPLES_VERSION = v1.3.1
+PTC_EXAMPLES_VERSION = v1.4.0-rc1
 PTC_EXAMPLES_SITE = $(call github,linux4sam,ptc_examples,$(PTC_EXAMPLES_VERSION))
 PTC_EXAMPLES_LICENSE = Apache-2.0 (programs), Microchip (firmware and config files)
 PTC_EXAMPLES_LICENSE_FILES = src/COPYING cfg/LICENCE.ptc_cfg fw/LICENCE.ptc_fw
-PTC_EXAMPLES_DEPENDENCIES = libgpiod libevdev
+PTC_EXAMPLES_DEPENDENCIES = libgpiod2 libevdev
 
 ifeq ($(BR2_PACKAGE_PTC_EXAMPLES_PLATFORM),"SAMA5D27_WLSOM1_EK")
-TARGET_CFLAGS += -DSAMA5D27_WLSOM1_EK
+PTC_EXAMPLES_CONF_OPTS += -DSAMA5D27_WLSOM1_EK=ON
 endif
 
-define PTC_EXAMPLES_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
-endef
-
-define PTC_EXAMPLES_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/src/ptc_qt*_demo $(TARGET_DIR)/usr/bin
-	$(INSTALL) -D -m 0755 $(@D)/src/start_ptc_qt* $(TARGET_DIR)/root
+define PTC_EXAMPLES_INSTALL_FIRMWARE
 	mkdir -p $(TARGET_DIR)/lib/firmware/microchip
 	$(INSTALL) -D -m 0644 $(@D)/cfg/*.bin $(TARGET_DIR)/lib/firmware/microchip
 	$(INSTALL) -D -m 0644 $(@D)/fw/*.bin $(TARGET_DIR)/lib/firmware/microchip
 endef
 
-$(eval $(generic-package))
+define PTC_EXAMPLES_INSTALL_SCRIPTS
+	$(INSTALL) -D -m 0755 $(@D)/src/start_ptc_qt* $(TARGET_DIR)/root
+endef
+
+PTC_EXAMPLES_POST_INSTALL_TARGET_HOOKS += PTC_EXAMPLES_INSTALL_FIRMWARE PTC_EXAMPLES_INSTALL_SCRIPTS
+
+$(eval $(cmake-package))
