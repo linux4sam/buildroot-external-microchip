3cf69351d4e7719fc23a0e801aec3b2871e53e56
Cyrille Pitchen <cyrille.pitchen@microchip.com>
2026-03-31 16:02:05 +0200

    package/libm2d: bump libm2d to v2.2.0
    Bump libm2d to v2.2.0, which introduces the support of the Vivante
    GC520UL 2D GPU embedded on the SAMA7D65 SoC and also now makes the
    difference between the GFX2D versions on SAM9X60 and SAM9X7 SoCs.
    
    Signed-off-by: Cyrille Pitchen <cyrille.pitchen@microchip.com>
    Reviewed-by: Nayab Sayed <nayabbasha.sayed@microchip.com>
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- package/libm2d/libm2d.mk
+++ package/libm2d/libm2d.mk
@@ -4,11 +4,31 @@
 #
 ################################################################################
 
-LIBM2D_VERSION = v2.1.0-rc2
+LIBM2D_VERSION = v2.2.0
 LIBM2D_SITE = $(call github,linux4sam,libm2d,$(LIBM2D_VERSION))
 LIBM2D_LICENSE = MIT
 LIBM2D_LICENSE_FILES = COPYING
-LIBM2D_DEPENDENCIES = libdrm host-pkgconf
+LIBM2D_DEPENDENCIES = host-pkgconf
 LIBM2D_INSTALL_STAGING = YES
+
+ifeq ($(BR2_MCHP_SAM_GC520UL_NANO2D),y)
+LIBM2D_CONF_OPTS += -DGPU=vivante,gc-nano2d
+LIBM2D_DEPENDENCIES += nano2d
+else ifeq ($(BR2_MCHP_SAM_GC520UL_ETNAVIV),y)
+LIBM2D_CONF_OPTS += -DGPU=vivante,gc-etnaviv
+LIBM2D_DEPENDENCIES += libdrm
+else ifeq ($(BR2_MCHP_SAM_SAM9X7_GFX2D),y)
+LIBM2D_CONF_OPTS += -DGPU=microchip,sam9x7-gfx2d
+LIBM2D_DEPENDENCIES += libdrm
+else
+LIBM2D_CONF_OPTS += -DGPU=microchip,sam9x60-gfx2d
+LIBM2D_DEPENDENCIES += libdrm
+endif
+
+ifeq ($(BR2_PACKAGE_LIBM2D_TESTS),y)
+LIBM2D_CONF_OPTS += -DENABLE_TESTS=ON
+else
+LIBM2D_CONF_OPTS += -DENABLE_TESTS=OFF
+endif
 
 $(eval $(cmake-package))
