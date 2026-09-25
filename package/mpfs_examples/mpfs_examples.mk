8b7432ede2898eb23171a180c0933bfbb16ee445
Jamie Gibbons <jamie.gibbons@microchip.com>
2026-09-25 12:08:11 +0100

    package/mpfs_examples: guard ethernet init install steps
    The mpfs_examples package unconditionally installed and renamed ethernet
    iio-http-server init scripts inside MPFS_EXAMPLES_INSTALL_INIT_SYSV and
    MPFS_EXAMPLES_INSTALL_INIT_SYSTEMD.
    
    When ethernet is not included in BR2_PACKAGE_MPFS_EXAMPLES_DIRS, the
    ethernet example directory is not copied into the target filesystem,
    causing build failures.
    
    Guard the ethernet-specific init installation and rename operations
    behind the same ethernet directory selection check already used for the
    iiohttpserver symlink creation.
    
    This allows partial example selections to build correctly without
    requiring the ethernet examples to be enabled.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>

--- package/mpfs_examples/mpfs_examples.mk
+++ package/mpfs_examples/mpfs_examples.mk
@@ -11,7 +11,7 @@ MPFS_EXAMPLE_DIRS = $(call qstrip,$(BR2_PACKAGE_MPFS_EXAMPLES_DIRS))
 MPFS_EXAMPLE_FILES_ALL += amp/rpmsg-pingpong/rpmsg-pingpong amp/rpmsg-tty-example/rpmsg-tty gpio/gpiod-test gpio/gpio-event system-services/system-services-example system-services/signature-verification-demo fpga-fabric-interfaces/lsram/uio-lsram-read-write dma/uio-dma-interrupt pdma/pdma-ex
 MPFS_EXAMPLE_TARGET_DIR = /opt/microchip/
 MPFS_EXAMPLE_FILES = \
-    $(foreach d,$(MPFS_EXAMPLE_DIRS),$(filter $(d)/%,$(MPFS_EXAMPLE_FILES_ALL)))
+	$(foreach d,$(MPFS_EXAMPLE_DIRS),$(filter $(d)/%,$(MPFS_EXAMPLE_FILES_ALL)))
 
 define MPFS_EXAMPLES_INSTALL_DIRS
 	$(foreach d,$(MPFS_EXAMPLE_DIRS), \
@@ -39,18 +39,22 @@ define MPFS_EXAMPLES_INSTALL_TARGET_CMDS
 endef
 
 define MPFS_EXAMPLES_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 $(@D)/ethernet/iio-http-server/collection/collectdiio.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/collectdiio.service
+	if echo "$(MPFS_EXAMPLE_DIRS)" | grep -wq ethernet; then \
+		$(INSTALL) -D -m 644 \
+			$(@D)/ethernet/iio-http-server/collection/collectdiio.service \
+			$(TARGET_DIR)/usr/lib/systemd/system/collectdiio.service; \
+	fi
 endef
 
 define MPFS_EXAMPLES_INSTALL_INIT_SYSV
-	# iiohttp server
-	$(INSTALL) -D -m 775 $(@D)/ethernet/iio-http-server/collection/collectdiio.busybox \
-		$(TARGET_DIR)/etc/init.d/collectdiio
-	# busy box init requires script renames
-	mv $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{sh,systemd}
-	mv $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{busybox,sh}
-	chmod +x $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.sh
+	if echo "$(MPFS_EXAMPLE_DIRS)" | grep -wq ethernet; then \
+		$(INSTALL) -D -m 775 \
+			$(@D)/ethernet/iio-http-server/collection/collectdiio.busybox \
+			$(TARGET_DIR)/etc/init.d/collectdiio; \
+		mv $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{sh,systemd}; \
+		mv $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.{busybox,sh}; \
+		chmod +x $(TARGET_DIR)$(MPFS_EXAMPLE_TARGET_DIR)ethernet/iio-http-server/run.sh; \
+	fi
 endef
 
 $(eval $(generic-package))
